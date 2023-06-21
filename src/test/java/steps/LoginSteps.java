3b03507a5aff1d05eb82f2350195c69785917019
sohai <SyntaxTechnologies>
2023-06-22 05:08:02 +0530

    lc2206

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,8 +3,12 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class LoginSteps {
 
@@ -16,24 +20,27 @@ public class LoginSteps {
         driver = new ChromeDriver();
         driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @When("user enters valid admin username and password")
     public void user_enters_valid_admin_username_and_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement usernameField = driver.findElement(By.id("txtUsername"));
+        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        //entering the credentials
+        usernameField.sendKeys("admin");
+        passwordField.sendKeys("Hum@nhrm123");
+
     }
 
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebElement loginButton = driver.findElement(By.name("Submit"));
+        loginButton.click();
     }
 
     @Then("user is successfully logged in the application")
     public void user_is_successfully_logged_in_the_application() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("My test case is passed");
     }
-
 }
