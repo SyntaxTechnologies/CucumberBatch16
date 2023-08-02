8392ee0aa2f4f0f4f104aa53b91e7a080e67e875
sohai <SyntaxTechnologies>
2023-08-02 05:30:26 +0530

    lc0801

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -1,7 +1,49 @@
 package API;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class HardCodedExamples {
+
+    //baseURI = baseURL + endpoint
+    //given - preparation
+    //when - hitting the endpoint
+    //base URI = base URL
+
+    String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
+    // value of token should be same as postman
+    String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2OTA5MzIzNzEsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY5MDk3NTU3MSwidXNlcklkIjoiNTYxNSJ9.xQAoWkfKQTzhnTUSOwi-f0WLMZxmxusDR_yjUHw8E8s";
+
+    //in this method we are going to create an employee
+    //we need headers, body to prepare the request
+    @Test
+    public void createEmployee(){
+        //preparing the request
+    RequestSpecification request = given().header("Content-Type","application/json").
+                header("Authorization", token).body("{\n" +
+                        "  \"emp_firstname\": \"justin\",\n" +
+                        "  \"emp_lastname\": \"azzuri\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"M\",\n" +
+                        "  \"emp_birthday\": \"2000-07-21\",\n" +
+                        "  \"emp_status\": \"happy\",\n" +
+                        "  \"emp_job_title\": \"QA\"\n" +
+                        "}");
+    //hitting the endpoint
+          Response response = request.when().post("/createEmployee.php");
+    //verifying the response
+          response.then().assertThat().statusCode(201);
+      //  System.out.println(response);
+        //this method we use to print the response of API in console
+        response.prettyPrint();
+    }
+
 
 
 
 }
+
