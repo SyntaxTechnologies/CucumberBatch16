d1b7c1f1807512ed123009dfc9dc263c1b33d757
sohai <SyntaxTechnologies>
2023-08-03 07:04:19 +0530

    lc0802

--- src/test/java/APIStepDef/APIWorkFlowSteps.java
+++ src/test/java/APIStepDef/APIWorkFlowSteps.java
@@ -5,13 +5,16 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class APIWorkFlowSteps {
 
     RequestSpecification request;
     Response response;
+    public static String employee_id;
 
     @Given("a request is prepared for creating an employee")
     public void a_request_is_prepared_for_creating_an_employee() {
@@ -38,4 +41,39 @@ public class APIWorkFlowSteps {
         response.then().assertThat().statusCode(statusCode);
     }
 
+
+    @Then("the employee created contains key {string} and value {string}")
+    public void the_employee_created_contains_key_and_value(String key, String value) {
+       response.then().assertThat().body(key, equalTo(value));
+    }
+
+    @Then("the employee id {string} is stored as a global variable")
+    public void the_employee_id_is_stored_as_a_global_variable(String empId) {
+        employee_id = response.jsonPath().getString(empId);
+        System.out.println(employee_id);
+    }
+//-----------------------------------------------------------------------------------------
+
+
+    @Given("a request is prepared for retrieving an employee")
+    public void a_request_is_prepared_for_retrieving_an_employee() {
+        request = given().header("Authorization",GenerateTokenStep.token)
+                .queryParam("employee_id", employee_id);
+    }
+
+    @When("a GET call is made to retrieve the employee")
+    public void a_get_call_is_made_to_retrieve_the_employee() {
+        response = request.when().get("/getOneEmployee.php");
+    }
+
+    @Then("the status code for this employee is {int}")
+    public void the_status_code_for_this_employee_is(int statusCode) {
+        response.then().assertThat().statusCode(statusCode);
+    }
+
+    @Then("the employee id {string} must match with globally stored employee id")
+    public void the_employee_id_must_match_with_globally_stored_employee_id(String empId) {
+        String temporaryEmpid = response.jsonPath().getString(empId);
+        Assert.assertEquals(employee_id, temporaryEmpid);
+    }
 }
