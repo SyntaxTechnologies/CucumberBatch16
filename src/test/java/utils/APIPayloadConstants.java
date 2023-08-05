ccd8c88d59e2d7e74f57eecaf533155020fe6c91
sohai <SyntaxTechnologies>
2023-08-05 21:44:13 +0530

    lc0805

--- src/test/java/utils/APIPayloadConstants.java
+++ src/test/java/utils/APIPayloadConstants.java
@@ -1,4 +1,19 @@
 package utils;
 
 public class APIPayloadConstants {
+
+    public static String createEmployeePayload(){
+        String createEmployeePayload = "{\n" +
+                "  \"emp_firstname\": \"justin\",\n" +
+                "  \"emp_lastname\": \"azzuri\",\n" +
+                "  \"emp_middle_name\": \"ms\",\n" +
+                "  \"emp_gender\": \"M\",\n" +
+                "  \"emp_birthday\": \"2000-07-21\",\n" +
+                "  \"emp_status\": \"happy\",\n" +
+                "  \"emp_job_title\": \"QA\"\n" +
+                "}";
+        return createEmployeePayload;
+    }
+
+
 }
