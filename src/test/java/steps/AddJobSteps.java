f6d65dc2cba22468adf7692751de807a8c0522eb
Asghar <instructor@syntaxtechs.com>
2023-08-07 19:49:55 -0400

    DB Testing Add Jobs

--- src/test/java/steps/AddJobSteps.java
+++ src/test/java/steps/AddJobSteps.java
@@ -8,6 +8,9 @@ import static steps.PageInitializer.jobPage;
 
 public class AddJobSteps extends CommonMethods {
 
+    String jTitle;
+    String jDesc;
+    String jNote;
     @Then("user clicks on the admin button")
     public void user_clicks_on_the_admin_button() {
     click(dashboardPage.adminButton);
@@ -22,24 +25,27 @@ public class AddJobSteps extends CommonMethods {
     }
     @Then("user clicks on the add button")
     public void user_clicks_on_the_add_button() {
-
+        click(jobPage.addButton);
     }
     @Then("user enters job {string} title")
-    public void user_enters_job_title(String string) {
-
+    public void user_enters_job_title(String title) {
+        sendText(title,jobPage.jobTitleF);
+        jTitle=title;
     }
     @Then("user enters job description {string}")
-    public void user_enters_job_description(String string) {
-
+    public void user_enters_job_description(String jobDescription) {
+        sendText(jobDescription,jobPage.jobDescF);
+        jDesc=jobDescription;
     }
 
     @Then("user enters job note {string}")
-    public void user_enters_job_note(String string) {
-
+    public void user_enters_job_note(String note) {
+        sendText(note,jobPage.jobNoteF);
+        jNote=note;
     }
     @Then("user clicks on the save button")
     public void user_clicks_on_the_save_button() {
-
+        click(jobPage.jobSvBtn);
     }
     @Then("verify data is stored properly in database")
     public void verify_data_is_stored_properly_in_database() {
