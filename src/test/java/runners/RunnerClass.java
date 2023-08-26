de44b3ac2318575df40489b6ee3ce242c08fccd2
sohai <SyntaxTechnologies>
2023-08-26 20:39:19 +0530

    latestcommit2608

--- src/test/java/runners/RunnerClass.java
+++ src/test/java/runners/RunnerClass.java
@@ -26,10 +26,6 @@ import org.junit.runner.RunWith;
       plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json",
                 "rerun:target/failed.txt"}
 
-
-
-
-
 )
 
 public class RunnerClass {
